c7cfcace40fa459da2044157feeb8ee14faca487
Shiba Murmu <shibamurmu001@gmail.com>
2025-11-20 18:12:28 +0530

    profile picture demo changed.

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 function Profile() {
     return (
         <>
-            <div className='grid h-screen bg-(--bg-color)  grid-col-2 md:grid-cols-2 md:gap-5 md:px-30 md:justify-center md:items-center p-10'>
+            <div className='grid h-screen bg-(--bg-color) items-center  grid-col-2 md:grid-cols-2 md:gap-5 md:px-30 md:justify-center md:items-center p-10'>
                 <div className='flex md:order-2 flex-col items-center '>
                     <div>
-                        <img src="https://media.istockphoto.com/id/1682296067/photo/happy-studio-portrait-or-professional-man-real-estate-agent-or-asian-businessman-smile-for.jpg?s=612x612&w=0&k=20&c=9zbG2-9fl741fbTWw5fNgcEEe4ll-JegrGlQQ6m54rg=" alt="profile" className='rounded-full 
+                        <img src="https://png.pngtree.com/png-vector/20230903/ourmid/pngtree-3d-illustration-avatar-profile-man-png-image_9945226.png" alt="profile" className='rounded-md
                         h-40 w-40 md:h-80 md:w-80' />
                     </div>
                     <div className='text-center mt-5'>
