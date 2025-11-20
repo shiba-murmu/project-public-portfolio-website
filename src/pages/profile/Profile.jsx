b8ad0402603d2f422c5fe37271db789c59b6e86a
Shiba Murmu <shibamurmu001@gmail.com>
2025-11-20 18:53:06 +0530

    added some more animation libraries like css libraries and scrolling library...

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Profile() {
     return (
         <>
-            <div className='grid h-screen bg-(--bg-color) items-center  grid-col-2 md:grid-cols-2 md:gap-5 md:px-30 md:justify-center md:items-center p-10'>
+            <div className='grid h-screen bg-(--bg-color) items-center mt-10 md:mt-0  grid-col-2 md:grid-cols-2 md:gap-5 md:px-30 md:justify-center md:items-center md:p-10 '>
                 <div className='flex md:order-2 flex-col items-center '>
                     <div>
                         <img src="https://png.pngtree.com/png-vector/20230903/ourmid/pngtree-3d-illustration-avatar-profile-man-png-image_9945226.png" alt="profile" className='rounded-md
@@ -24,7 +24,7 @@ function Profile() {
                         </div>
                     </div>
                 </div>
-                <div className='md:order-1 flex md:pl-20 flex-col  justify-left items-start md:gap-4'>
+                <div className='md:order-1  h-96 flex md:pl-20 md:flex-col  md:justify-left  md:gap-4'>
                     <div>
                         {/* For desktop screen */}
                         <div className='hidden md:block text-5xl regular-bolder font-extrabold'>
@@ -32,6 +32,7 @@ function Profile() {
                         </div>
                     </div>
                     <div>
+                        {/* Not visible to mobile */}
                         <h3 className='hidden md:block text-2xl text-neutral-500 '>Frontend Developer</h3>
                     </div>
                     <div>
@@ -40,7 +41,8 @@ function Profile() {
                         </p>
                     </div>
                     <div className='hidden md:flex gap-5 mt-4 text-(--bluish) font-semibold'>
-                        <div className='cursor-pointer bg-(--bluish) text-white px-4 py-2 rounded-md hover:bg-(--bluish) transition duration-300'>
+                        {/* not visible to mobile */}
+                        <div className='cursor-pointer bg-(--bluish) text-(--bluish-text) px-4 py-2 rounded-md hover:bg-(--bluish) transition duration-300'>
                             View work
                         </div>
                         <div className='cursor-pointer text-black border border-gray-500 px-4 py-2 rounded-md hover:shadow-md  transition duration-300'>
@@ -48,13 +50,13 @@ function Profile() {
                         </div>
                     </div>
                     <div className='mt-5 hidden md:block'>
+                        {/* not visible to mobile */}
                         <div className='flex flex-wrap gap-3'>
-                            <span className='bg-gray-200 text-gray-800 px-3 py-1 rounded-full text-sm'>Python</span>  
-                            <span className='bg-gray-200 text-gray-800 px-3 py-1 rounded-full text-sm'>JavaScript</span>  
-                            <span className='bg-gray-200 text-gray-800 px-3 py-1 rounded-full text-sm'>React</span>  
-                            <span className='bg-gray-200 text-gray-800 px-3 py-1 rounded-full text-sm'>Tailwind</span>  
+                            <span className='bg-(--bluish) text-(--bluish-text) px-3 py-1 rounded-full text-sm'>Python</span>
+                            <span className='bg-(--bluish) text-(--bluish-text) px-3 py-1 rounded-full text-sm'>JavaScript</span>
+                            <span className='bg-(--bluish) text-(--bluish-text) px-3 py-1 rounded-full text-sm'>React</span>
+                            <span className='bg-(--bluish) text-(--bluish-text) px-3 py-1 rounded-full text-sm'>Tailwind</span>
                         </div>
-
                     </div>
                 </div>
             </div>
