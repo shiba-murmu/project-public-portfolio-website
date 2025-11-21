116c6abde506db05f90e6dd0e77f98b0b5a7f350
Shiba Murmu <shibamurmu001@gmail.com>
2025-11-21 21:43:45 +0530

    proejct card designed in the desktop size screen..

--- src/components/project_card/Project_card_desktop.jsx
+++ src/components/project_card/Project_card_desktop.jsx
@@ -1,13 +1,41 @@
 import React from 'react'
-
-function Project_card_desktop() {
-  return (
-    <>
-        <div className='min-h-screen'>
-            Desktop view
-        </div>
-    </>
-  )
+import { Link } from 'react-router-dom'
+function Project_card_desktop({ orderOne, orderTwo , path }) {
+    return (
+        <>
+            <div className='grid grid-cols-2 w-full gap-50  h-96'>
+                <div className={`flex order-${orderOne}  justify-center items-center `}>
+                    <img src={path}
+                        className=' object-contain h-full w-full'
+                        alt="" />
+                </div>
+                <div className={`flex order-${orderTwo} justify-center items-start flex-col`}>
+                    <div className='border-b-2 border-(--bluish) py-2 px-20'>
+                        <span className='text-3xl font-bold text-(--bluish)'>Hotel management</span>
+                    </div>
+                    <div>
+                        <span className='font-extrabold text-xl'>Tech used : </span>
+                        <ul className='flex text-lg font-bold pl-5 gap-1 text-(--bluish)'>
+                            <li>Html,</li>
+                            <li>CSS,</li>
+                            <li>Javascript,</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <span className='font-extrabold text-xl'>Description : </span>
+                        <p className='text-gray-500 text-lg pl-5'>Project description goes here</p>
+                    </div>
+                    <div className='cursor-pointer flex px-30 justify-center items-center space-x-4 mt-auto pt-3 pb-3 rounded-md'>
+                        <Link to='/project-details'>
+                            <button className='bg-(--bluish) text-white px-4 py-2 rounded-md'>Details</button>
+                        </Link>
+                        <button className='cursor-pointer bg-(--bluish) text-white px-4 py-2 rounded-md'>Code</button>
+                        <button className='cursor-pointer bg-(--bluish) text-white px-4 py-2 rounded-md'>Live</button>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Project_card_desktop
--- src/pages/projects/Project_list.jsx/Project_collection.jsx
+++ src/pages/projects/Project_list.jsx/Project_collection.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import Project_card from '../../../components/project_card/Project_card'
-
+import Project_card_desktop from '../../../components/project_card/Project_card_desktop'
 function Project_collection() {
   return (
    <>
-    <div className='min-h-screen bg-(--about-bg) flex flex-col justify-start  items-center'>
+    <div className=' md:w-[170vh] min-h-screen flex flex-col justify-start md:justify-start items-center '>
         <div className='flex md:hidden mt-5 border-b-2 border-(--bluish) py-3 px-4 rounded-b-2xl shadow-md  justify-center items-center'>
             <span className='text-3xl md:text-5xl font-extrabold text-(--bluish)'>Projects collection</span>
         </div>
@@ -14,9 +14,10 @@ function Project_collection() {
             <Project_card />
             <Project_card />
         </div>
-        <div>
+        <div className='hidden md:flex md:flex-col md:gap-40 w-full py-10 '>
             {/* Designed for desktop screen */}
-
+            <Project_card_desktop orderOne={1} orderTwo={2} path='https://projectsly.com/images/blog/best-project-design.png?v=1686553999071005322'/>
+            <Project_card_desktop orderOne={2} orderTwo={1} path='https://projectsly.com/images/blog/best-project-design.png?v=1686553999071005322' />
         </div>
     </div>
    </>
