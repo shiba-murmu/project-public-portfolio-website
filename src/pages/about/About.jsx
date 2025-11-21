be3c9849b4b8ea1a27b436bba4358017304252d5
Shiba Murmu <shibamurmu001@gmail.com>
2025-11-21 17:14:57 +0530

    quick facts in the about section added.

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -3,7 +3,7 @@ import Heading_text from '../../components/Heading_text/Heading_text'
 function About() {
     return (
         <>
-            <div id='About' className='min-h-screen md:pl-50 flex flex-col justify-center md:justify-start items-center md:items-start text-3xl font-bold'>
+            <div id='About' className='min-h-screen md:pl-50 flex flex-col justify-center md:justify-start items-center md:items-start text-3xl font-semibold'>
                 <div className=''>
                     <Heading_text text='About me' />
                 </div>
@@ -11,15 +11,15 @@ function About() {
                     <section id="about" className="p-6 rounded-2xl" style={{ background: '' }}>
                         <div className="mt-3 md:flex md:flex-col md:gap-6">
                             <p
-                                className="text-sm md:text-lg                                                                              md:flex-1 leading-relaxed"
-                                
+                                className="text-sm md:text-lg                                                                              md:flex-1 leading-relaxed text-center md:text-left"
+
                             >
                                 I'm <span className="font-extrabold heading-text text-xl md:text-2xl text-(--bluish)">Shiba Murmu</span> — a frontend engineer who loves building
                                 clean, fast and intuitive web experiences. I started coding during my B.Tech in Computer
                                 Science, and over time I found myself drawn toward creating polished user interfaces and
                                 solving real user-experience problems.
                                 <br /><br />
-                                I enjoy working with <span className="font-semibold">React, Tailwind CSS, and Firebase</span>, and I’m always
+                                I enjoy working with <span className="font-extrabold text-(--bluish) ">React, Tailwind CSS, and Firebase</span>, and I’m always
                                 exploring tools that help create smoother and more human-friendly digital products. My focus
                                 is simple: build interfaces that are lightweight, responsive, and visually consistent —
                                 without unnecessary complexity.
@@ -29,13 +29,15 @@ function About() {
                                 from scratch.
                             </p>
 
-                            <div className="mt-6 md:mt-0 md:w-64">
-                                <h4 className="font-extrabold regular-bolder  text-md md:text-3xl mb-2">Quick Facts</h4>
-                                <ul className="space-y-1 text-sm" style={{ color: 'var(--muted)' }}>
-                                    <li>B.tech in Computer Science</li>
-
-                                 
-                                </ul>
+                            <div className="mt-6 flex flex-col gap-2 justify-center items-center md:justify-start md:items-start md:mt-0 md:w-full">
+                                <h4 className="font-extrabold text-md md:text-3xl mb-2">Quick Facts</h4>
+                                <ol className=" text-center md:text-start list-inside space-y-1 text-sm md:text-lg" style={{ color: 'var(--muted)' }}>
+                                    <li>B.Tech in Computer Science</li>
+                                    <li>2+ years of experience</li>
+                                    <li>Passionate about UI/UX design</li>
+                                    <li>Based in India</li>
+                                    <li>Open to freelance & full-time work</li>
+                                </ol>
                             </div>
                         </div>
                     </section>
