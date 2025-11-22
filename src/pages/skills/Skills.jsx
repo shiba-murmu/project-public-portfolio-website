522754e3372b4387e7a11026df8d8d581b12ebff
Shiba Murmu <shibamurmu001@gmail.com>
2025-11-22 20:57:05 +0530

    remoevd code.

--- src/pages/skills/Skills.jsx
+++ src/pages/skills/Skills.jsx
@@ -3,12 +3,12 @@ import Heading_text from '../../components/heading_text/Heading_text'
 function Skills() {
     return (
         <>
-            <div id='skills' className='min-h-screen flex justify-center items-start pt-10 text-4xl font-bold  '>
+            <div id='skills' className='min-h-screen flex justify-center items-start pt-10 font-bold  '>
                 <div className='flex justify-center items-center'>
                     <Heading_text text='Technical skills' />
                 </div>
                 <div>
-
+                    
                 </div>
             </div>
         </>
