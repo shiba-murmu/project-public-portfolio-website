eba72f8547b433a9a150232477070713d14ea970
Shiba Murmu <shibamurmu001@gmail.com>
2025-11-23 10:05:22 +0530

    semiclone added

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,5 +1,6 @@
 import React , { useState , useEffect} from 'react'
-import Heading_text from '../../components/heading_text/Heading_text'
+import Heading_text from '../../components/heading_text/Heading_text';
+
 function About() {
     const [name , setName] = useState('No name')
     const [lastName , setLastName] = useState(null)
