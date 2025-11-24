56be21ff38e5eacd069cbfd1a21d3fb0fc1ad823
Shiba Murmu <shibamurmu001@gmail.com>
2025-11-24 07:37:26 +0530

    Footer page is designed for the desktop version

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,13 +1,60 @@
 import React from 'react'
 
 function Footer() {
-  return (
-    <>
-        <div id='Footer'  className='p-4 text-center bg-(--footer-background)'>
-            © 2023 Shiba Murmu. All rights reserved.
-        </div>
-    </>
-  )
+    return (
+        <>
+            <div id='Footer' className=' md:rounded-t-4xl md:flex md:flex-col md:gap-20  bg-(--footer-background) md:p-30 md:pb-10'>
+                <div className='md:grid md:grid-cols-2 md:justify-center md:content-start'>
+                    <div className='md:flex md:flex-col md:gap-3'>
+                        <section className='md:flex md:flex-col md:gap-3'>
+                            <span>CONTACT ME</span>
+                            <span className='md:text-6xl'>
+                                Let's Discuss  <br />  about   work
+                            </span>
+                        </section>
+                        {/* Here one more section can be added to call the person directly */}
+                        <section>
+                            EMAIL ME AT
+                        </section>
+                        <section className='md:mt-4'>
+                            <span className='border p-3 rounded-full px-10 border-dashed'>shibamurmu001@gmail.com</span>
+                        </section>
+                    </div>
+                    <div className='md:grid md:grid-cols-2 md:justify-center md:content-start'>
+                        <div>
+                            <ul className='md:flex md:flex-col md:gap-2'>
+                                <li>QUICK LINK</li>
+                                <li>Home</li>
+                                <li>About me</li>
+                                <li>Projects</li>
+                                <li>Technical skills</li>
+                                <li>Experience</li>
+                                <li>Contact</li>
+                            </ul>
+                        </div>
+                        <div>
+                            <ul className='md:flex md:flex-col md:gap-2'>
+                                <li>INFORMATION</li>
+                                <li>Terms and service</li>
+                                <li>Privacy policy</li>
+                                <li>Cookie settings</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div className='border-t border-(--muted-text) md:pt-10 md:grid md:grid-cols-2'>
+                    {/* Other works and link of social media */}
+                    <section className=''>
+                        © PORTFOLIO 2025. ALL RIGHTS RESERVED
+                    </section>
+                    <section className=' md:flex md:justify-end'>
+                        {/* Here we add social media links */}
+                        laksdfj
+                    </section>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Footer
