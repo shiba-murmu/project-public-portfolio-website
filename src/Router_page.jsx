a75c5d4bb12b83e59945b7691f4e04d7ad9156a1
Shiba Murmu <shibamurmu001@gmail.com>
2025-11-26 20:32:19 +0530

    Some codes is added or some paths is also added to te router page

--- src/Router_page.jsx
+++ src/Router_page.jsx
@@ -9,10 +9,27 @@ import CustomHeader from './components/header/CustomHeader.jsx';
 import Project_details from './pages/projects/project_detail/Project_details.jsx';
 import Admin_login from './admin/Admin_login.jsx';
 import AdminDashboard from './admin/AdminDashboard.jsx';
+import Admin_add from './admin/Admin_add.jsx';
+import Add_user from './admin/Add_user.jsx';
 function Router_page() {
     const location = useLocation();
-    const hideNavbarRoutes = ['/project-collections' , '/project-details', '/admin-and-users', '/admin-dashboard'];
-    const hidefooterRoutes = ['/project-details', '/project-collections', '/admin-and-users', '/admin-dashboard'];
+    const hideNavbarRoutes = [
+        '/project-collections',
+        '/project-details',
+        '/admin-and-users',
+        '/admin-dashboard',
+        '/add-admin',
+        '/add-user',
+    ];
+
+    const hidefooterRoutes = [
+        '/project-details',
+        '/project-collections',
+        '/admin-and-users',
+        '/admin-dashboard',
+        '/add-admin',
+        '/add-user',
+    ];
 
     const shouldHideNavbar = hideNavbarRoutes.includes(location.pathname);
     const shouldHideFooter = hidefooterRoutes.includes(location.pathname);
@@ -25,8 +42,10 @@ function Router_page() {
                 <Route path="/" element={<Home />} />
                 <Route path='/project-collections' element={<Project_collection />} />
                 <Route path='/project-details' element={<Project_details />} />
-                <Route path='/admin-and-users' element={ <Admin_login />} />
+                <Route path='/admin-and-users' element={<Admin_login />} />
                 <Route path='/admin-dashboard' element={<AdminDashboard />} />
+                <Route path='/add-admin' element={<Admin_add />} />
+                <Route path='/add-user' element={<Add_user />} />
             </Routes>
             {!shouldHideFooter && <Footer />}
         </>
