0e11b6dc6dc1b168d20a3ec9404f1458a1bbcc00
Shiba Murmu <shibamurmu001@gmail.com>
2025-11-27 07:24:06 +0530

    User project data has been created

---
+++ src/admin/UserMyWorkData.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function UserMyWorkData() {
+    return (
+        <>
+            <div>
+                User my work data
+            </div>
+        </>
+    )
+}
+
+export default UserMyWorkData
